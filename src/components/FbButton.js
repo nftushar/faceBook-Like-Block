37006359ebee773688b9455ceb3e848ec8f88d79
nftushar <nftushar5200@gmail.com>
2023-12-07 17:12:13 +0600

    colorscheme done adjucted

--- src/components/FbButton.js
+++ src/components/FbButton.js
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 function FbButton() {
-  const [likeButtonHtml, setLikeButtonHtml] = useState('');
-  const [shareButtonHtml, setShareButtonHtml] = useState('');
+    const [likeButtonHtml, setLikeButtonHtml] = useState('');
+    const [shareButtonHtml, setShareButtonHtml] = useState('');
 
-  useEffect(() => {
-    const dynamicUrl = "https://www.facebook.com/profile.php?id=100076228742365";
-    const dynamicLayout = "box_count";
-    const dynamicColorscheme = "dark";
-    const dynamicWidth = 250;
-    const dynamicHeight = 60;
+    useEffect(() => {
+        const dynamicUrl = "https://www.facebook.com/profile.php?id=100076228742365";
+        const dynamicLayout = "box_count";
+        const dynamicColorscheme = "dark";
+        const dynamicWidth = 250;
+        const dynamicHeight = 60;
 
-    const likeHtml = `<iframe src="https://www.facebook.com/plugins/like.php?href=${dynamicUrl}&layout=${dynamicLayout}&show_faces=false&action=like&font=verdana&colorscheme=${dynamicColorscheme}" allowtransparency="true" style="border: none; overflow: hidden; width: ${dynamicWidth}px; height: ${dynamicHeight}px;" frameborder="0" scrolling="no" data-width="${dynamicWidth}" data-height="${dynamicHeight}"></iframe>`;
+        const likeHtml = `<iframe src="https://www.facebook.com/plugins/like.php?href=${dynamicUrl}&layout=${dynamicLayout}&size=large&show_faces=false&action=like&font=verdana&colorscheme=${dynamicColorscheme}" allowtransparency="true" style="border: none; overflow: hidden; width: ${dynamicWidth}px; height: ${dynamicHeight}px;" frameborder="0" scrolling="no" data-width="${dynamicWidth}" data-height="${dynamicHeight}"></iframe>`;
 
-    const shareHtml = `<iframe src="https://www.facebook.com/plugins/share_button.php?href=${dynamicUrl}&layout=button&size=large&width=100&height=30&appId=" width="10" height="30" style="border:none;overflow:hidden;" scrolling="no" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
 
-    setLikeButtonHtml(likeHtml);
-    setShareButtonHtml(shareHtml);
-  }, []);
+        const shareHtml = `<iframe src="https://www.facebook.com/plugins/share_button.php?href=${dynamicUrl}&layout=button&size=large&colorscheme=${dynamicColorscheme}&width=100&height=30&appId=" style="border:none;overflow:hidden;width:${dynamicWidth}px;height:${dynamicHeight}px;" scrolling="no" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
 
-  return (
-    <div className="fb-button-container">
-      <div className="fb-like-button" dangerouslySetInnerHTML={{ __html: likeButtonHtml }}></div>
-      <div className="fb-share-button" dangerouslySetInnerHTML={{ __html: shareButtonHtml }}></div>
-    </div>
-  );
+        setLikeButtonHtml(likeHtml);
+        setShareButtonHtml(shareHtml);
+    }, []);
+
+    return (
+        <div className="fb-button-container">
+            <div className="fb-like-button" dangerouslySetInnerHTML={{ __html: likeButtonHtml }}></div>
+            <div className="fb-share-button" dangerouslySetInnerHTML={{ __html: shareButtonHtml }}></div>
+        </div>
+    );
 }
 
 export default FbButton;
