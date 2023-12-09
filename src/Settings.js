3bbbbc6555c59f5004cae2e551aa8ab1b190b3d3
nftushar <nftushar5200@gmail.com>
2023-12-09 16:55:48 +0600

    dynamic share button done

--- src/Settings.js
+++ src/Settings.js
@@ -2,7 +2,7 @@
 // import { useState } from 'react';
 import { __ } from "@wordpress/i18n";
 import { InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, TabPanel, TextControl, RangeControl, __experimentalUnitControl as UnitControl, __experimentalBoxControl as BoxControl, ToggleControl, SelectControl } from "@wordpress/components";
+import { PanelBody, TabPanel, TextControl, RangeControl, __experimentalBoxControl as BoxControl, ToggleControl, SelectControl } from "@wordpress/components";
 
 import { Background, BorderControl, BtnGroup } from "../../Components";
 import { produce } from 'immer';
@@ -14,20 +14,9 @@ const mapAlignments = [
 ];
 
 const Settings = ({ attributes, setAttributes }) => {
-	const { background, filters, hovFilters, padding, zoom, height, width, border, alignment,   btnType, fbUrl, layout, clrScheme, size } = attributes;
+	const { background, filters, hovFilters, padding, zoom, height, width, border, alignment, btnType, fbUrl, layout, clrScheme, size, shareOffOn } = attributes;
 
-	// console.log(btnType);
-
-	const updateObj = (attr, key, val, nestKey = false) => {
-		const newObj = produce(attributes[attr], draft => {
-			if (false !== nestKey) {
-				draft[key][nestKey] = val;
-			} else {
-				draft[key] = val;
-			}
-		});
-		setAttributes({ [attr]: newObj })
-	}
+	// console.log(btnType); 
 
 	return (
 		<InspectorControls>
@@ -36,8 +25,8 @@ const Settings = ({ attributes, setAttributes }) => {
 				tabs={[
 					{ name: "general", title: __("General") },
 					{ name: "style", title: __("Style") }
-				]}
-			>
+				]}>
+
 				{(tab) => <>
 					{tab.name === "general" && <PanelBody
 						className="bPlPanelBody"
@@ -46,6 +35,12 @@ const Settings = ({ attributes, setAttributes }) => {
 							label={__("FaceBook", "fb-button")}
 							value={fbUrl}
 							onChange={(val) => setAttributes({ fbUrl: val })}
+						/>
+						<ToggleControl
+							label={__("Share Button", "map-block")}
+							value={shareOffOn}
+							checked={shareOffOn}
+							onChange={(val) => setAttributes({ shareOffOn: val })} 
 						/>
 						<SelectControl
 							label="Layout"
@@ -85,36 +80,13 @@ const Settings = ({ attributes, setAttributes }) => {
 							]}
 							onChange={(val) => setAttributes({ size: val })}
 							isIcon={true} />
-
-						<RangeControl
-							className="mt20"
-							label={__("Zoom", "fb-button")}
-							value={zoom}
-							onChange={(val) => setAttributes({ zoom: val })}
-							min={0}
-							max={20}
-							step={1}
-						/>
 					</PanelBody>}
 
 
 					{tab.name === "style" && <>
 						<PanelBody
 							className="bPlPanelBody"
-							title={__("Map", "fb-button")}>
-							<UnitControl
-								label={__("Width", "fb-button")}
-								labelPosition="left"
-								value={width}
-								onChange={(val) => setAttributes({ width: val })}
-							/>
-							<UnitControl
-								className='mt20'
-								label={__("Height", "fb-button")}
-								labelPosition="left"
-								value={height}
-								onChange={(val) => setAttributes({ height: val })}
-							/>
+							title={__("Button Control", "fb-button")}>
 
 							<BtnGroup
 								className="mt20"
@@ -142,134 +114,7 @@ const Settings = ({ attributes, setAttributes }) => {
 								}}
 								onChange={(value) => setAttributes({ padding: value })}
 							/>
-
-							<BorderControl label={__('Border:', 'fb-button')}
-								value={border} onChange={val => setAttributes({ border: val })}
-								defaults={{ radius: '5px' }} />
 						</PanelBody>
-
-
-						<PanelBody className="bPlPanelBody" title={__("Filter", "fb-button")} initialOpen={false}>
-							<TabPanel className="bPlTabPanel"
-								tabs={[
-									{ name: "normal", title: __("Normal") },
-									{ name: "hover", title: __("Hover") },
-								]} >
-
-								{(tab) => <>
-									{tab.name === "normal" && <PanelBody className="bPlPanelBody" >
-										<RangeControl
-											label={__("Blur", "fb-button")}
-											value={filters.blur}
-											onChange={(val) => updateObj('filters', 'blur', val)}
-											min={0}
-											max={10}
-											step={0.1}
-											allowReset={true}
-											resetFallbackValue={0}
-										/>
-										<RangeControl
-											label={__("Brightness", "fb-button")}
-											value={filters.brightness}
-											onChange={(val) => updateObj('filters', 'brightness', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={100}
-										/>
-										<RangeControl
-											label={__("Contrast", "fb-button")}
-											value={filters.contrast}
-											onChange={(val) => updateObj('filters', 'contrast', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={100}
-										/>
-										<RangeControl
-											label={__("Saturate", "fb-button")}
-											value={filters.saturate}
-											onChange={(val) => updateObj('filters', 'saturate', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={100}
-										/>
-										<RangeControl
-											label={__("Hue", "fb-button")}
-											value={filters.hue}
-											onChange={(val) => updateObj('filters', 'hue', val)}
-											min={0}
-											max={360}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={1}
-										/>
-									</PanelBody>}
-
-
-									{tab.name === "hover" && <PanelBody className="bPlPanelBody">
-
-										<RangeControl
-											label={__("Blur", "fb-button")}
-											value={hovFilters.blur}
-											onChange={(val) => updateObj('hovFilters', 'blur', val)}
-											min={0}
-											max={10}
-											step={0.1}
-											allowReset={true}
-											resetFallbackValue={0}
-										/>
-
-										<RangeControl
-											label={__("Brightness", "fb-button")}
-											value={hovFilters.brightness}
-											onChange={(val) => updateObj('hovFilters', 'brightness', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={60}
-										/>
-										<RangeControl
-											label={__("Contrast", "fb-button")}
-											value={hovFilters.contrast}
-											onChange={(val) => updateObj('hovFilters', 'contrast', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={150}
-										/>
-										<RangeControl
-											label={__("Saturate", "fb-button")}
-											value={hovFilters.saturate}
-											onChange={(val) => updateObj('hovFilters', 'saturate', val)}
-											min={0}
-											max={200}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={200}
-										/>
-										<RangeControl
-											label={__("Hue", "fb-button")}
-											value={hovFilters.hue}
-											onChange={(val) => updateObj('hovFilters', 'hue', val)}
-											min={0}
-											max={360}
-											step={1}
-											allowReset={true}
-											resetFallbackValue={10}
-										/>
-									</PanelBody>}
-								</>}
-							</TabPanel>
-						</PanelBody>
-
-
 					</>}
 				</>}
 			</TabPanel>
--- src/components/FbButton.js
+++ src/components/FbButton.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
 function FbButton({ attributes }) {
-    const { fbUrl, btnType, layout, clrScheme, size, border, shareOffOn } = attributes;
+    const { fbUrl, btnType, layout, clrScheme, size, shareOffOn } = attributes;
 
-    // console.log(btnType);
+    console.log(shareOffOn);
 
     const [buttonHtml, setButtonHtml] = useState('');
 
@@ -15,7 +15,9 @@ function FbButton({ attributes }) {
         src="https://www.facebook.com/v2.10/plugins/like.php?action=${btnType}&app_id=&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df1160b0232558e4%26domain%3Dlocalhost%26is_canvas%3Dfalse%26origin%3Dhttp%253A%252F%252Flocalhost%252Fffa86e1338ac3%26relation%3Dparent.parent&amp;color_scheme=${clrScheme}&amp;container_width=140&amp;href=${fbUrl}&amp;layout=${layout}&amp;locale=en_US&amp;sdk=joey&amp;share=${shareOffOn}&amp;show_faces=false&amp;size=${size}" style="border: none; visibility: visible; width:${iframeWidth}px; height: ${iframeHeight}px;" class=""></iframe>`;
 
         setButtonHtml(buttonHtml);
-    }, [layout, clrScheme, btnType]); // Include btnType in the dependency array
+    }, [layout, clrScheme, btnType, shareOffOn]); // Include btnType in the dependency array
+ 
+
 
     return (
         <div className="fb-button-container">
